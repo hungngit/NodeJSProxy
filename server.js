3d9c7e31cc7f5f008f93b6b85e337a0f75b696ea
Nguyen Thanh Hung <Nguyen Thanh Hung>
2016-04-07 10:45:14 +0700

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
-var httpProxy = require('http-proxy');
-var bodyParser = require('body-parser');
+var express = require('express'),
+	httpProxy = require('http-proxy'),
+	bodyParser = require('body-parser'),
+	urldecode = require('urldecode'),
+	validUrl = require('valid-url'),
+	parse_url = require('url').parse;
 
 // Solution for forwarding from http to https taken from:
 // http://stackoverflow.com/questions/15801014/how-to-use-node-http-proxy-for-http-to-https-routing
@@ -21,10 +24,20 @@ var apiProxy = httpProxy.createProxyServer(proxyOptions);
 var server = express();
 server.use(express.static(__dirname + '/public'));
 
-server.all("/proxy*", function(req, res) {
-	console.log('------------------------------------');
-	console.log(req.headers);
-	apiProxy.web(req, res, { target: req.query.url});
+server.all('/proxy*', function(req, res) {
+	if (!req.query.url){
+		res.send('Please, put parameter url with encode!');
+		return;
+	}
+	var url = urldecode(req.query.url);
+	if (!validUrl.isUri(url)){
+		res.send('Please, parameter url must be uri!');
+		return;
+	}
+	if (parse_url(req.query.url).path.indexOf('?') < 0){
+		url += '?';
+	}
+	apiProxy.web(req, res, { target: url });
 });
 
 server.use(bodyParser.json());
